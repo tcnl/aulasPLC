0f373daa7e2450e603a009fff325ca0f5d1c8cf5
Cinthya Moreira Lins <cml2@cin.ufpe.br>
2018-09-18 13:01:06 -0300

    page 40

--- livro.hs
+++ livro.hs
@@ -96,3 +96,17 @@ max' a b
 --     | a > b = GT
 --     | a == b = EQ
 --     | otherwise = LT
+
+bmiTell'' :: (RealFloat a) => a -> a -> String
+bmiTell'' weight height
+    | bmi <= skinny = "You're underweight, you emo, you!"
+    | bmi <= normal = "You're supposedly normal. Pfft, I bet you're ugly!"
+    | bmi  <= fat = "You're fat! Lose some weight, fatty!"
+    | otherwise = "You're a whale,congratulations!"
+    where   bmi = weight / height ^ 2
+            skinny = 18.5
+            normal = 25.0
+            fat = 30.0
+
+-- *Main> bmiTell'' 65 1.55
+-- "You're fat! Lose some weight, fatty!"
