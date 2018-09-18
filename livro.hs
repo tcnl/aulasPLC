906882e8ec2c6cc9532778be6bdba5532ca1f701
Cinthya Moreira Lins <cml2@cin.ufpe.br>
2018-09-18 11:38:31 -0300

    page 37

--- livro.hs
+++ livro.hs
@@ -22,3 +22,43 @@ sayMe 3 = "Three!"
 sayMe 4 = "Four!"
 sayMe 5 = "Five!"
 sayMe x = "Not between 1 and 5" 
+
+-- factorial2 :: (Integral a) => a -> a
+factorial2 0 = 1
+factorial2 n = n * factorial (n - 1)
+
+addVectors :: (Num a) => (a,a) -> (a,a) -> (a,a)
+addVectors a b = (fst a + fst b, snd a + snd b)
+
+first :: (a,b,c) -> a
+first (x,_,_) = x
+second :: (a,b,c) -> b
+second (_,y,_) = y
+third :: (a,b,c) -> c
+third (_,_,z) = z
+
+-- *Main> let xs = [(1,3), (4,3), (2,4)]
+-- *Main> [a+b | (a,b) <- xs]
+-- [4,7,6]
+
+head' :: [a] -> a
+head' [] = error "Cant't call head on an empty list, dummy!"
+head' (x:_) = x
+
+tell :: (Show a) => [a] -> String
+tell [] = "The list is empty"
+tell (x:[]) = "The list has one element: " ++ show x
+tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y
+tell (x:y:_) = "The list is long. The first two elements are: " ++ show x ++ " and " ++ show y
+-- *Main> tell [1,2,3,4,5]
+-- "The list is long. The first two elements are: 1 and 2"
+-- *Main> tell [1]
+-- "The list has one element: 1"
+
+length' :: (Num b) => [a] -> b
+length' [] = 0
+length'(_:xs) = 1 + length' xs
+
+sum' :: (Num a) => [a] -> a
+sum' [] = 0
+sum' (x:xs) = x + sum' xs
