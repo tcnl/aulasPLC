072337b42d861a2db8488964e2f82d8381d9eb28
Cinthya Moreira Lins <cml2@ug3c17.windows.cin.ufpe.br>
2018-09-27 12:57:56 -0300

    questao 2-a

--- monitoria27_09_2018.hs
+++ monitoria27_09_2018.hs
@@ -1,3 +1,4 @@
+import Data.List(sort)
 -- Questão 1 EE1 2018.1
 -- Defina operadores de seção s1 e s2 de maneira que
 -- filter (>5) . map (+2)
@@ -6,10 +7,25 @@
 --         x >= 3
 -- então:
 -- f = filter (>5) . map (+2)
+-- f [1..5]
+-- [6,7]
 -- g = map (+2) . filter (>3)
+-- g [1..5]
+-- [6,7]
 
 -- Questão 2 EE1 2018.1
+-- a)
+data CInt = Conjunto [Int] deriving (Show)
 
+getInts :: CInt -> [Int]
+getInts (Conjunto a) = a
 
+makeSet :: [Int] -> CInt
+makeSet [] = Conjunto []
+makeSet (x:xs) = Conjunto . sort $ if x `notElem` xs then x:l else l
+    where l = getInts (makeSet xs)
+
+-- *Main> makeSet [11,11,3,4,5,2,2,3]
+-- Conjunto [2,3,4,5,11]
 
 
