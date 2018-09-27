c70ae041f271912ab3d4f8de8efbb18dfa801bb5
Cinthya Moreira Lins <cml2@ug3c17.windows.cin.ufpe.br>
2018-09-27 12:28:34 -0300

    moni q1

---
+++ monitoria27_09_2018.hs
@@ -0,0 +1,15 @@
+-- Questão 1 EE1 2018.1
+-- Defina operadores de seção s1 e s2 de maneira que
+-- filter (>5) . map (+2)
+-- sol.:   x + 2 > 5
+--         x > 5 - 2
+--         x >= 3
+-- então:
+-- f = filter (>5) . map (+2)
+-- g = map (+2) . filter (>3)
+
+-- Questão 2 EE1 2018.1
+
+
+
+
