8abcbc4749d8d8a91110b766d59797112a11f210
Thiago Casa Nova da Luz <tcnl@ug1c40.windows.cin.ufpe.br>
2018-10-04 16:02:32 -0300

    Main

--- aula04_10.hs
+++ aula04_10.hs
@@ -3,3 +3,15 @@
                  putStrLn (reverse l)
                  putStrLn (reverse l)
                  putStrLn (reverse l)
+
+main = do getLine
+   line <- getLine
+   if null line
+   then (do
+          l <- return "String Vazia"
+          putStrLn l 
+        )
+    else (do
+        putStrLn $ reverseWords line
+        main
+        )
